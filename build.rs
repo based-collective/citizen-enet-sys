51965870059154f771b444104e691e92a517a755
Jujhar Singh <jujharsmc@gmail.com>
2022-03-15 15:42:53 +1300

    thing

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
-extern crate cmake;
 extern crate bindgen;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
-use cmake::Config;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
@@ -12,6 +11,54 @@ fn main() {
         .clang_arg("-Ivendor/enet/include/")
         .header("wrapper.h")
         .derive_debug(false)
+        .blocklist_type("LPMONITORINFOEXA?W?")
+        .blocklist_type("LPTOP_LEVEL_EXCEPTION_FILTER")
+        .blocklist_type("MONITORINFOEXA?W?")
+        .blocklist_type("PEXCEPTION_FILTER")
+        .blocklist_type("PEXCEPTION_ROUTINE")
+        .blocklist_type("PSLIST_HEADER")
+        .blocklist_type("PTOP_LEVEL_EXCEPTION_FILTER")
+        .blocklist_type("PVECTORED_EXCEPTION_HANDLER")
+        .blocklist_type("_?L?P?CONTEXT")
+        .blocklist_type("_?L?P?EXCEPTION_POINTERS")
+        .blocklist_type("_?P?DISPATCHER_CONTEXT")
+        .blocklist_type("_?P?EXCEPTION_REGISTRATION_RECORD")
+        .blocklist_type("_?P?IMAGE_TLS_DIRECTORY.*")
+        .blocklist_type("_?P?NT_TIB")
+        .blocklist_type("tagMONITORINFOEXA")
+        .blocklist_type("tagMONITORINFOEXW")
+        .blocklist_function("AddVectoredContinueHandler")
+        .blocklist_function("AddVectoredExceptionHandler")
+        .blocklist_function("CopyContext")
+        .blocklist_function("GetThreadContext")
+        .blocklist_function("GetXStateFeaturesMask")
+        .blocklist_function("InitializeContext")
+        .blocklist_function("InitializeContext2")
+        .blocklist_function("InitializeSListHead")
+        .blocklist_function("InterlockedFlushSList")
+        .blocklist_function("InterlockedPopEntrySList")
+        .blocklist_function("InterlockedPushEntrySList")
+        .blocklist_function("InterlockedPushListSListEx")
+        .blocklist_function("LocateXStateFeature")
+        .blocklist_function("QueryDepthSList")
+        .blocklist_function("RaiseFailFastException")
+        .blocklist_function("RtlCaptureContext")
+        .blocklist_function("RtlCaptureContext2")
+        .blocklist_function("RtlFirstEntrySList")
+        .blocklist_function("RtlInitializeSListHead")
+        .blocklist_function("RtlInterlockedFlushSList")
+        .blocklist_function("RtlInterlockedPopEntrySList")
+        .blocklist_function("RtlInterlockedPushEntrySList")
+        .blocklist_function("RtlInterlockedPushListSListEx")
+        .blocklist_function("RtlQueryDepthSList")
+        .blocklist_function("RtlRestoreContext")
+        .blocklist_function("RtlUnwindEx")
+        .blocklist_function("RtlVirtualUnwind")
+        .blocklist_function("SetThreadContext")
+        .blocklist_function("SetUnhandledExceptionFilter")
+        .blocklist_function("SetXStateFeaturesMask")
+        .blocklist_function("UnhandledExceptionFilter")
+        .blocklist_function("__C_specific_handler")
         .generate()
         .expect("Unable to generate bindings");
 
@@ -20,22 +67,18 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 
-    
-    let dst = Config::new("vendor/enet")
-                .build_target("enet")
-                .build();
-
-    eprintln!("LUL: {}", dst.display());
-
-    if target.contains("windows") {
-        if is_debug {
-            println!("cargo:rustc-link-search=native={}/build/Debug", dst.display());
-        } else {
-            println!("cargo:rustc-link-search=native={}/build/Release", dst.display());
-        }
-        println!("cargo:rustc-link-lib=dylib=winmm");
-    } else {
-        println!("cargo:rustc-link-search=native={}/build", dst.display());
-    }
-    println!("cargo:rustc-link-lib=static=enet");
+    let dst = cc::Build::new()
+        .cpp(true)
+        .include("vendor/enet/include/")
+        .files(vec![
+            "vendor/enet/src/callbacks.cpp",
+            "vendor/enet/src/compress.cpp",
+            "vendor/enet/src/host.cpp",
+            "vendor/enet/src/list.cpp",
+            "vendor/enet/src/packet.cpp",
+            "vendor/enet/src/peer.cpp",
+            "vendor/enet/src/protocol.cpp",
+            "vendor/enet/src/win32.cpp",
+            ])
+        .compile("enet");
 }
